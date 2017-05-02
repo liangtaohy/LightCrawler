ae1f6ac8432a0b4ff305b63861c7ed42cb017f55
LiangTao <liangtao@xmanlegal.com>
2017-05-02 16:25:51 +0800

    新疆金融办公室

---
+++ worker/SpiderJrbXinJiangGov.class.php
@@ -0,0 +1,110 @@
+<?php
+
+/**
+ * 新疆金融办公室
+ * http://jrb.xinjiang.gov.cn/tzgg/index.htm
+ * User: Liang Tao (liangtaohy@163.com)
+ * Date: 17/5/2
+ * Time: PM4:16
+ */
+define("CRAWLER_NAME", md5("spider-bjjrj.gov"));
+require_once dirname(__FILE__) . "/../includes/lightcrawler.inc.php";
+
+class SpiderJrbXinJiangGov extends SpiderFrame
+{
+    const MAGIC = __CLASS__;
+
+    /**
+     * Seed Conf
+     * @var array
+     */
+    static $SeedConf = array(
+        "http://jrb\.xinjiang\.gov\.cn/tzgg/index\.htm",
+    );
+
+    protected $ContentHandlers = array(
+        "#http://jrb\.xinjiang\.gov\.cn/tzgg/index([0-9_]+)?\.htm# i" => "handleListPage",
+        "#/[0-9]{4,6}/[0-9]+\.htm# i"  => "handleDetailPage",
+        "#/[0-9a-zA-Z_]+\.(doc|pdf|txt|xls)# i" => "handleAttachment",
+    );
+
+    /**
+     * SpiderJrbXinJiangGov constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @param PHPCrawlerDocumentInfo $DocInfo
+     * @return bool|XlegalLawContentRecord
+     */
+    protected function _handleDetailPage(PHPCrawlerDocumentInfo $DocInfo)
+    {
+        $source = $DocInfo->source;
+
+        $extract = new ExtractContent($DocInfo->url, $DocInfo->url, $source);
+
+        $extract->parse();
+
+        $content = $extract->getContent();
+        $c = preg_replace("/[\s\x{3000}]+/u", "", $content);
+        $record = new XlegalLawContentRecord();
+        $record->doc_id = md5($c);
+        $record->title = !empty($extract->title) ? $extract->title : $extract->guessTitle();
+        $record->author = $extract->author;
+        $record->content = $content;
+        $record->doc_ori_no = $extract->doc_ori_no;
+        $record->publish_time = $extract->publish_time;
+        $record->t_valid = $extract->t_valid;
+        $record->t_invalid = $extract->t_invalid;
+        $record->negs = implode(",", $extract->negs);
+        $record->tags = $extract->tags;
+        $record->simhash = '';
+        if (!empty($extract->attachments)) {
+            $record->attachment = json_encode($extract->attachments, JSON_UNESCAPED_UNICODE);
+        }
+
+        if (empty(gsettings()->debug)) {
+            $res = FlaskRestClient::GetInstance()->simHash($c);
+
+            $simhash = '';
+            if (isset($res['simhash']) && !empty($res['simhash'])) {
+                $simhash = $res['simhash'];
+            }
+
+            if (isset($res['repeated']) && !empty($res['repeated'])) {
+                echo 'data repeated: ' . $DocInfo->url . ', repeated simhash: ' . $res['simhash1'] .PHP_EOL;
+                $flag = 1;
+                if (!empty($record->doc_ori_no)) {
+                    $r = DaoXlegalLawContentRecord::getInstance()->ifDocOriExisted($record);
+                    if (empty($r)) {
+                        $flag = 0;
+                    }
+                }
+
+                if ($flag)
+                    return false;
+            }
+
+            $record->simhash = $simhash;
+        }
+
+
+        $record->type = DaoSpiderlLawBase::TYPE_TXT;
+        $record->status = 1;
+        $record->url = $extract->baseurl;
+        $record->url_md5 = md5($extract->url);
+
+        if (gsettings()->debug) {
+            //echo "raw: " . implode("\n", $extract->text) . PHP_EOL;
+            //$index_blocks = $extract->indexBlock($extract->text);
+            //echo implode("\n", $index_blocks) . PHP_EOL;
+            var_dump($record);
+            exit(0);
+        }
+        echo "insert data: " . $record->doc_id . PHP_EOL;
+        return $record;
+    }
+}
